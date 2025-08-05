477d4e411fa9506cee3a84f25f3840105e170f65
Sandeep Kumar <sandkr2093@gmail.com>
2025-08-05 23:29:46 +0530

    Add files via upload

---
+++ y67.py
@@ -0,0 +1,20 @@
+class Shape:
+    def draw(s):
+        print("Drawing a shape")
+class Circle(Shape):
+    def draw(Shape):
+        print("Drawing a Circle")
+class Square(Shape):
+    def draw(s):
+        print("Drawing a Square")
+class Triangle(Shape):
+    def draw(s):
+        print("Drawing a Triangle")
+obj = Triangle()
+obj.draw()
+obj = Circle()
+obj.draw()
+obj = Square()
+obj.draw()
+obj = Shape()
+obj.draw()
