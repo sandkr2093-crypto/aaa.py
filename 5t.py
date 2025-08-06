ed15801b5c300a9aa9a7c314d9bceb64ebbbe354
Sandeep Kumar <sandkr2093@gmail.com>
2025-08-06 13:01:54 +0530

    Add files via upload

---
+++ 5t.py
@@ -0,0 +1,25 @@
+Age = [18,16,25,27,26,24,48,46,50,57,65]
+temp = Age[1]
+Age[1] = Age[10]
+Age[10] = temp
+print(Age)
+
+
+
+
+Age = [20,28,16,14,26,35,34,47,45,48,51,56,65,74,85,95]
+temp = Age[3]
+Age[3] = Age[15]
+Age[15] = temp
+print(Age)
+
+
+
+
+
+
+Price = [120,184,154,256,247,365,312,458,589]
+temp = Price[0]
+Price[0] = Price[8]
+Price[8] = temp
+print(Price)
