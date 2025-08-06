5e7fac78457f07474246f4d28f5d0df6eb0e375b
Sandeep Kumar <sandkr2093@gmail.com>
2025-08-06 21:52:28 +0530

    Add files via upload

---
+++ 5u.py
@@ -0,0 +1,8 @@
+a = int(input("enter the first number"))
+b = int(input("enter the second number"))
+if a>b:
+    print("first number is greater")
+elif b>a:
+    print("second number is greater")
+else:
+    print("both numbers are equal")
