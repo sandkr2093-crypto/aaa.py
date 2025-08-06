9ca8073439ffaa006bec9a71df9be035072ed6f4
Sandeep Kumar <sandkr2093@gmail.com>
2025-08-06 21:43:54 +0530

    Add files via upload

---
+++ er.py
@@ -0,0 +1,9 @@
+marks = int(input("enter the marks"))
+if marks>=90:
+    print("Grade A")
+elif marks>=75 and marks<=89:
+    print("Grade B")
+elif marks>=60 and marks<=74:
+    print("Grade C")
+else:
+    print("Fail")
