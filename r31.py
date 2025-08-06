b76d9c73d74184fba2f1a7ec9ece0851fe3da92e
Sandeep Kumar <sandkr2093@gmail.com>
2025-08-06 12:41:28 +0530

    Add files via upload

---
+++ r31.py
@@ -0,0 +1,5 @@
+List = [48,12,15,20,28,35,34,47,59,68,74,89,95,100]
+List.sort()
+print("second largest number is",List[-2])
+print("third largest number is",List[-3])
+print("fourth largest number is",List[-4])
