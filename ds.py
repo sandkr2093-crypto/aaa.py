3057c84c9172f0dbe571d50f940094ea5aaf38c0
Sandeep Kumar <sandkr2093@gmail.com>
2025-08-06 22:45:51 +0530

    Add files via upload

---
+++ ds.py
@@ -0,0 +1,6 @@
+Employees = {"Name":"Alok","Age":45,"Dept":"IT","Salary":38000,"City":"Gurugram"}
+print(Employees["Name"])
+print(Employees["Age"])
+print(Employees["Dept"])
+print(Employees["Salary"])
+print(Employees["City"])
